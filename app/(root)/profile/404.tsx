9ae5c54abc92d73f287a495e2bc0eb856ac404f4
6410110338 <125024387+6410110338@users.noreply.github.com>
2025-01-07 15:48:55 +0700

    profile_error_page

--- app/(root)/profile/404.tsx
+++ app/(root)/profile/404.tsx
@@ -1,4 +1,4 @@
-// app/404.tsx หรือ pages/404.tsx
+import Thai from "@/dictionary/thai";
 import React from "react";
 
 interface ErrorPageProps {
@@ -11,13 +11,15 @@ const ErrorPage: React.FC<ErrorPageProps> = ({ error, reset }) => {
     <div className="error-page flex flex-col gap-16 items-center justify-center min-h-screen">
       <div className="flex gap-2 divide-red-950 divide-x-2">
         <h1 className="font-san text-6xl text-red-600">404</h1>
-        <h1 className="font-semibold text-6xl text-red-600">{error.message}</h1>
+        <h1 className="font-semibold text-6xl text-red-600 ">
+          {error.message}
+        </h1>
       </div>
       <button
         onClick={reset}
         className="btn-retry p-3 w-[100px] h-[50px] outline outline-red-700 rounded-lg hover:text-red-700  "
       >
-        Try Again
+        {Thai.Back}
       </button>
     </div>
   );
